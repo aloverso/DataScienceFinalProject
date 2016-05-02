22bad1805cffd1d7c2d03f6f82a823abf655def4
Pratool Gadtaula <pratool.gadtaula@students.olin.edu>
2016-05-01 20:39:14 -0400

    Implemented heat-map level highlighting

--- district_mapping/d3_helpers.js
+++ district_mapping/d3_helpers.js
@@ -1,19 +1,17 @@
 var queue = d3_queue.queue;
 
-function highlight(countryNames, regionName) {
+function highlight(state, dist) {
   d3.selectAll("text").filter(".regionName")
-    .text(regionName);
-  countryNames.forEach(function(d) {
-    d3.select("path[title=\"" + d + "\"]")
-      .attr("class", "highlight");
-  });
+    .text(state + " " + dist.toString());
+  
+  d3.select("path[state=\"" + state + "\"][dist=\"" + dist + "\"]")
+    .style("fill", "black")
+    .attr("class", "highlight");
 }
 
-function unhighlight(countryNames) {
+function unhighlight(state, dist, maxinc, colorScale) {
   d3.selectAll("text").filter(".regionName")
     .text("");
-  countryNames.forEach(function(d) {
-    d3.select("path[title=\"" + d + "\"]")
-      .attr("class", "country");
-  });
+  d3.select("path[state=\"" + state + "\"][dist=\"" + dist + "\"]")
+    .style("fill", colorScale(maxinc));
 }
--- district_mapping/districts.js
+++ district_mapping/districts.js
@@ -1,9 +1,12 @@
 var mapVisualization = function() {
   var width = 500;
   var height = 600;
+  
+  var colors = ["#fcfbfd","#efedf5","#dadaeb","#bcbddc","#9e9ac8","#807dba","#6a51a3","#54278f","#3f007d"];
+  var buckets = 9;
 
   var projection = d3.geo.mercator()
-    .translate([1700, 700])
+    .translate([1200, 700])
     .scale(600);
 
   var path = d3.geo.path()
@@ -17,49 +20,61 @@ var mapVisualization = function() {
   var g = svg.append("g");
   
   queue()
-    .defer(d3.json, "https://raw.githubusercontent.com/dinopants174/DataScienceFinalProject/master/district_maps/districts060.json")
+    .defer(d3.json, "https://raw.githubusercontent.com/dinopants174/DataScienceFinalProject/master/district_maps/districts100.json")
     .defer(d3.json, "https://raw.githubusercontent.com/dinopants174/DataScienceFinalProject/district-mapping/district_mapping/reps-by-district.json")
     .await(ready);
   
+  var maxOfficeTime = 0;
+  
   function ready (error, counties, names) {
-    var counties = topojson.object(counties, counties.objects.out).geometries;
+    var districts = topojson.object(counties, counties.objects.out).geometries;
     
-    counties.forEach(function(d) {
-      filter = names['table'][17][d.properties.state];
+    districts.forEach(function(d) {
+      filter = names['table'][100][d.properties.state];
       if (filter != undefined) {
         filter = filter[d.properties.district];
         if (filter != undefined) {
           d.properties.names = [];
           d.properties.ids = [];
+          d.properties.incumbencies = [];
           filter.forEach(function(f) {
             d.properties.names.push(f[0]);
             d.properties.ids.push(f[1]);
-            console.log(f[2]);
+            d.properties.incumbencies.push(f[2]);
+            if (f[2] > maxOfficeTime) {
+              maxOfficeTime = f[2];
+            }
         });
         }
       }
     });
+    
+    var colorScale = d3.scale.quantile()
+              .domain([0, buckets - 1, maxOfficeTime])
+              .range(colors);
 
-    var county = svg.selectAll(".county").data(counties);
+    var county = svg.selectAll(".districts").data(districts);
     county.enter()
       .insert("path")
-      //.attr("class", "county")
       .attr("state", function(d,i) { return d.properties.state; })
       .attr("dist", function(d,i) { return d.properties.district; })
       .attr("names", function(d,i) { return d.properties.names; })
+      .attr("inc", function(d, i) { return d.properties.incumbencies; })
+      .attr("maxinc", function(d,i) { return Math.max(d.properties.incumbencies); })
+      .style("fill", function(d) { return colorScale( Math.max(d.properties.incumbencies) ); })
       .attr("class", function(d, i) {
         if (d.properties.names == undefined) {
-          return "county no-rep";
+          return "district no-rep";
         }
-        return "county";
+        return "district";
       })
       .attr("d", path);
     
-    /*svg.append("text")']
+    svg.append("text")
     .attr("class", "regionName")
     .attr("text-anchor", "end")
     .attr("x", width)
     .attr("y", height-30)
-    .text("");*/
+    .text("");
   }
 }
