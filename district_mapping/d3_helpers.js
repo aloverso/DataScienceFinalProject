b1afa4f5a8e18c76ffce04c98e778cc9ef1b3f98
Pratool Gadtaula <pratool.gadtaula@students.olin.edu>
2016-05-02 22:31:34 -0400

    Added a color scale that makes sense

--- district_mapping/d3_helpers.js
+++ district_mapping/d3_helpers.js
@@ -3,10 +3,23 @@ var queue = d3_queue.queue;
 var colors = ["#fcfbfd","#efedf5","#dadaeb","#bcbddc","#9e9ac8","#807dba","#6a51a3","#54278f","#3f007d"];
 var buckets = 9;
 
-function highlight(state, dist) {
-  var corner = state + ", District " + dist.toString();
-  d3.selectAll("text").filter(".regionName")
-    .text(corner);
+function highlight(state, dist, nameIncs) {
+  if (nameIncs == "undefined")
+    nameIncs = [];
+  var region = state + ", District " + dist.toString();
+  
+  var textArea = d3.selectAll("text").filter(".regionName");
+  textArea
+    .text(region);
+  
+  if (nameIncs != undefined) {
+    nameIncs.forEach(function (d) {
+      textArea.append("tspan")
+        .attr("x", "0")
+        .attr("dy", "1.5em")
+        .text(d[0] + ", " + d[1] + " years in office");
+    });
+  }
   
   d3.select("path[state=\"" + state + "\"][dist=\"" + dist + "\"]")
     .style("fill", "black");
--- district_mapping/districts.js
+++ district_mapping/districts.js
@@ -1,12 +1,12 @@
 var mapVisualization = function() {
-  var width = 500;
-  var height = 600;
+  var width = 600;
+  var height = 700;
   
   var colors = ["#fcfbfd","#efedf5","#dadaeb","#bcbddc","#9e9ac8","#807dba","#6a51a3","#54278f","#3f007d"];
   var buckets = 9;
 
   var projection = d3.geo.mercator()
-    .translate([1200, 700])
+    .translate([1300, 700])
     .scale(600);
 
   var path = d3.geo.path()
@@ -20,7 +20,7 @@ var mapVisualization = function() {
   var g = svg.append("g");
   
   queue()
-    .defer(d3.json, "https://raw.githubusercontent.com/dinopants174/DataScienceFinalProject/master/district_maps/districts100.json")
+    .defer(d3.json, "https://raw.githubusercontent.com/dinopants174/DataScienceFinalProject/master/district_maps/districts112.json")
     .defer(d3.json, "https://raw.githubusercontent.com/dinopants174/DataScienceFinalProject/district-mapping/district_mapping/reps-by-district.json")
     .await(ready);
   
@@ -30,21 +30,25 @@ var mapVisualization = function() {
     var districts = topojson.object(counties, counties.objects.out).geometries;
     
     districts.forEach(function(d) {
-      filter = names['table'][100][d.properties.state];
+      filter = names['table'][112][d.properties.state];
       if (filter != undefined) {
         filter = filter[d.properties.district];
         if (filter != undefined) {
           d.properties.names = [];
           d.properties.ids = [];
           d.properties.incumbencies = [];
+          d.properties.nameInc = [];
           filter.forEach(function(f) {
             d.properties.names.push(f[0]);
+            var nameIncTemp = [f[0], f[2]];
+            nameIncTemp.toString = function () { return "[\"" + this.join("\", \"") + "\"]"; }
+            d.properties.nameInc.push(nameIncTemp);
             d.properties.ids.push(f[1]);
             d.properties.incumbencies.push(f[2]);
             if (f[2] > maxOfficeTime) {
               maxOfficeTime = f[2];
             }
-        });
+          });
         }
       }
     });
@@ -58,8 +62,8 @@ var mapVisualization = function() {
       .insert("path")
       .attr("state", function(d,i) { return d.properties.state; })
       .attr("dist", function(d,i) { return d.properties.district; })
-      .attr("names", function(d,i) { return d.properties.names; })
-      .attr("inc", function(d, i) { return d.properties.incumbencies; })
+      //.attr("names", function(d,i) { return d.properties.names; })
+      //.attr("inc", function(d, i) { return d.properties.incumbencies; })
       .attr("maxinc", function(d,i) { return Math.max(d.properties.incumbencies); })
       .style("fill", function(d) { return colorScale( Math.max(d.properties.incumbencies) ); })
       .attr("class", function(d, i) {
@@ -68,16 +72,22 @@ var mapVisualization = function() {
         }
         return "district";
       })
-      .attr("onmouseover", function(d) { return "highlight(\""+d.properties.state+"\", \""+d.properties.district+"\")"; })
+      .attr("onmouseover", function(d) {
+        if (d.properties.nameInc != undefined) {
+          d.properties.nameInc.toString = function () { return "[" + this.join(", ") + "]"; }
+        }
+        return "highlight(\"" + d.properties.state + "\", \"" + d.properties.district + "\", " +
+          d.properties.nameInc + ")"; })
       .attr("onmouseout", function(d) {
-        return "unhighlight(\""+d.properties.state+"\", \""+d.properties.district+"\", \""+colorScale(Math.max(d.properties.incumbencies))+"\")"; })
+        return "unhighlight(\"" + d.properties.state + "\", \"" + d.properties.district + "\", \"" +
+          colorScale(Math.max(d.properties.incumbencies)) + "\")"; })
       .attr("d", path);
     
-    svg.append("text")
-    .attr("class", "regionName")
-    .attr("text-anchor", "end")
-    .attr("x", width)
-    .attr("y", height-30)
-    .text("");
+    svg.append("g")
+      .attr("transform", "translate("+(width/2)+", "+(height-250)+")")
+      .append("text")
+      .attr("class", "regionName")
+      .attr("text-anchor", "middle")
+      .text("");
   }
 }
