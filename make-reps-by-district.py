75281e3100b6bd0cedcb37dbcb1e9936e4e771c4
Pratool Gadtaula <pratool.gadtaula@students.olin.edu>
2016-05-04 21:18:44 -0400

    removed unnecessary district mapping files

--- make-reps-by-district.py
+++ make-reps-by-district.py
@@ -3,20 +3,19 @@ import pandas as pd
 
 def create_table():
     """
-    Creates a look up table to retrieve a Congress person's name given their
-    state, district, and session of Congress.
+    Creates a look up table to retrieve a Congress person's name, vote id,
+    and length of time in office given their state, district, and session
+    of Congress.
     """
 
     table = {}
     with open('states_hash.json', 'r') as state_file:
         states_hash = json.load(state_file)
-    congress = pd.read_csv('../legislators.csv', low_memory=False)
+    congress = pd.read_csv('./legislators.csv', low_memory=False)
     sessions = []
     for session_name in congress:
         if 'c' in session_name and len(session_name) < 5:
             sessions.append(session_name)
-    #states = congress['state'].unique()
-    #districts = range(1,60)
     for session in range(len(sessions)):
         current = congress[(congress[sessions[session]]==1) &\
                   (congress['type']=='rep')]
@@ -26,7 +25,6 @@ def create_table():
                           current['state'],\
                           current['district'],\
                           current['vote_id']):
-            #cong_name = sessions[session]
             name = member[0] + ' ' + member[1]
             state = states_hash[member[2]]
             dist = int(member[3])
@@ -39,7 +37,6 @@ def create_table():
                     incumbency_len += 2
                 else:
                     break
-
 
             if state in table[session]:
                 if dist in table[session][state]:
